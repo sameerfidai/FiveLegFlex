eb04ad37a115301155c5ff7e1ab33ec40d43fb06
Sameer Fidai <sameer.fidai09@gmail.com>
2024-05-09 19:57:41 -0400

    add spinner

--- api/main.py
+++ api/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from booksdata import getBestProps
 
 app = FastAPI()
+
+# Configure allowed origins
+origins = [
+    "http://localhost:3000",  # For local development
+    "https://five-leg-flex.vercel.app/",  # Replace with your deployed frontend URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/best-props")
--- src/pages/props.js
+++ src/pages/props.js
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "@/components/Layout";
-import NProgress from "nprogress";
 import BettingProp from "@/components/BettingProp";
-import "nprogress/nprogress.css";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
-const API_URL = "https://fivelegflex-backend.onrender.com";
-const LOCAL_URL = "http://localhost:8000/api/best-props";
+const API_URL = "https://fivelegflex-backend.onrender.com"; // For local testing
 
-export default function Home({ initialBettingProps }) {
-  const [bettingProps, setBettingProps] = useState(initialBettingProps);
+export default function PropsPage() {
+  const [bettingProps, setBettingProps] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchProps = async () => {
-      NProgress.start(); // Start the progress bar when the fetch begins
       try {
         const res = await fetch(`${API_URL}/api/best-props`);
-        //const res = await fetch(LOCAL_URL); // local
         if (!res.ok) throw new Error("Failed to fetch data");
         const data = await res.json();
         setBettingProps(data);
       } catch (error) {
         console.error("Error fetching data:", error);
-        setBettingProps((prevProps) => ({ ...prevProps, message: error.toString() }));
+        setBettingProps({ message: error.toString() });
+      } finally {
+        setLoading(false);
       }
-      NProgress.done(); // Stop the progress bar regardless of fetch success or failure
     };
 
     fetchProps();
   }, []);
+
+  if (loading) return <LoadingSpinner />;
 
   const hasProps = bettingProps && bettingProps.data && bettingProps.data.length > 0;
 
@@ -35,7 +35,7 @@ export default function Home({ initialBettingProps }) {
     <Layout>
       <div className="bg-fullblack text-white p-4 md:p-6 lg:p-8">
         {hasProps ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-12 place-items-stretch">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 place-items-stretch">
             {bettingProps.data.map((prop, index) => (
               <BettingProp key={index} prop={prop} />
             ))}
@@ -48,26 +48,4 @@ export default function Home({ initialBettingProps }) {
       </div>
     </Layout>
   );
-}
-
-// Fetch initial props using getStaticProps
-export async function getStaticProps() {
-  const props = await fetchInitialProps(); // Fetch initial props data
-  return {
-    props: {
-      initialBettingProps: props,
-    },
-  };
-}
-
-// Placeholder function to simulate fetching data for initial props
-async function fetchInitialProps() {
-  try {
-    const res = await fetch(`${API_URL}/api/best-props`);
-    // const res = await fetch(LOCAL_URL); // local
-    if (!res.ok) throw new Error("Failed to load the initial props.");
-    return await res.json();
-  } catch (error) {
-    return { data: [], message: "Error loading initial props: " + error.message };
-  }
 }
