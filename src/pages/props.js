6a557119058ff7eff7aa59cff1165f862385f7ca
Sameer Fidai <sameer.fidai09@gmail.com>
2024-05-09 17:02:49 -0400

    update api url for backend on render

--- src/pages/props.js
+++ src/pages/props.js
@@ -4,7 +4,8 @@ import NProgress from "nprogress";
 import BettingProp from "@/components/BettingProp";
 import "nprogress/nprogress.css";
 
-// Initial props are fetched at build time and passed to the Home component
+const API_URL = "https://fivelegflex-backend.onrender.com";
+
 export default function Home({ initialBettingProps }) {
   const [bettingProps, setBettingProps] = useState(initialBettingProps);
 
@@ -12,7 +13,7 @@ export default function Home({ initialBettingProps }) {
     const fetchProps = async () => {
       NProgress.start(); // Start the progress bar when the fetch begins
       try {
-        const res = await fetch("http://localhost:8000/api/best-props");
+        const res = await fetch(`${API_URL}/api/best-props`);
         if (!res.ok) throw new Error("Failed to fetch data");
         const data = await res.json();
         setBettingProps(data);
@@ -23,11 +24,7 @@ export default function Home({ initialBettingProps }) {
       NProgress.done(); // Stop the progress bar regardless of fetch success or failure
     };
 
-    // Optionally, set an interval or trigger based on user actions
-    // const fetchInterval = setInterval(fetchProps, 10000); // For example, fetch every 10 seconds
-
-    // Uncomment to enable periodic fetching
-    // return () => clearInterval(fetchInterval); // Clean up the interval on component unmount
+    fetchProps();
   }, []);
 
   const hasProps = bettingProps && bettingProps.data && bettingProps.data.length > 0;
@@ -53,20 +50,18 @@ export default function Home({ initialBettingProps }) {
 
 // Fetch initial props using getStaticProps
 export async function getStaticProps() {
-  // Simulate fetching data as you would in your existing getStaticProps
-  const props = await fetchInitialProps(); // This should contain the logic to fetch data similar to your previous getStaticProps
+  const props = await fetchInitialProps(); // Fetch initial props data
   return {
     props: {
       initialBettingProps: props,
     },
-    // revalidate: 10,
   };
 }
 
 // Placeholder function to simulate fetching data for initial props
 async function fetchInitialProps() {
   try {
-    const res = await fetch("http://localhost:8000/api/best-props");
+    const res = await fetch(`${API_URL}/api/best-props`);
     if (!res.ok) throw new Error("Failed to load the initial props.");
     return await res.json();
   } catch (error) {
