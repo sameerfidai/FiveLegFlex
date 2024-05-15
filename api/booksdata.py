a4eea490be6b6a65a533fcedc7fa9ccf765c416b
Sameer Fidai <sameer.fidai09@gmail.com>
2024-05-14 21:21:23 -0400

    Add caching to API functions to prevent rate limiting

--- api/booksdata.py
+++ api/booksdata.py
@@ -1,12 +1,77 @@
 from datetime import datetime, timezone
 import requests
+from cachetools import TTLCache, cached
 
-API_KEY = "917eeafc14e938bb43ab513e5e997981"
+API_KEY = "ff1d310c402ceb35517b12717c3b9faf"
 SPORT = "basketball_nba"
 REGIONS = "us"
 ODDS_FORMAT = "american"
 
+# create caches with a time-to-live (TTL) of 5 minutes (300 seconds)
+prizepicks_cache = TTLCache(maxsize=100, ttl=300)
+odds_cache = TTLCache(maxsize=100, ttl=300)
+games_cache = TTLCache(maxsize=100, ttl=300)
 
+
+@cached(prizepicks_cache)
+def getPrizePicksData():
+    """
+    Gets props data currently live on PrizePicks.
+
+    Returns:
+        dict: A dictionary mapping player ID to player data (name, lines, team, etc).
+    """
+
+    URL = "https://partner-api.prizepicks.com/projections?league_id=7"
+
+    try:
+        response = requests.get(URL)
+        if response.status_code != 200:
+            print(f"Failed to retrieve data: {response.status_code}, {response.text}")
+            return {}
+
+        try:
+            prizepicks_data = response.json()
+        except ValueError as e:
+            print(f"Error parsing JSON response: {e}")
+            return {}
+
+        if not prizepicks_data.get("data"):
+            print("No PrizePicks Data...")
+            return {}
+
+        lines = prizepicks_data["data"]
+        players_lines = {
+            player["id"]: player["attributes"]
+            for player in prizepicks_data["included"]
+            if player["type"] == "new_player"
+        }
+
+        for player_id in players_lines:
+            players_lines[player_id]["lines"] = {}
+
+        for line in lines:
+            attributes = line["attributes"]
+            odds_type = attributes.get("odds_type")
+
+            if odds_type in ["demon", "goblin"]:
+                continue
+
+            player_id = line["relationships"]["new_player"]["data"]["id"]
+            stat_type = attributes["stat_type"]
+            stat_line = attributes["line_score"]
+
+            if player_id in players_lines:
+                players_lines[player_id]["lines"][stat_type] = stat_line
+
+        return players_lines
+
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+        return {}
+
+
+@cached(games_cache)
 def getGames():
     """
     Fetches a list of event IDs for upcoming NBA games.
@@ -24,11 +89,9 @@ def getGames():
         response = requests.get(events_url, params=params)
         response.raise_for_status()
         events_data = response.json()
-        # print(events_data)
 
         if events_data:
             print(f"Retrieved {len(events_data)} events for {SPORT}.")
-            # filter games to include only those that have not yet started
             event_ids = [
                 event["id"]
                 for event in events_data
@@ -44,71 +107,80 @@ def getGames():
     return event_ids
 
 
-def getPrizePicksData():
+@cached(odds_cache)
+def getPlayersPropsOddsForGame(event_id, prop_type):
     """
-    Gets props data currently live on PrizePicks.
+    Retrieves betting odds for specified player propositions (e.g., points, assists, rebounds) from different bookmakers for a specific game.
+
+    Parameters:
+        event_id (str): The unique ID for the game.
+        prop_type (str): The type of player prop to retrieve odds for (e.g., "player_points", "player_assists", "player_rebounds").
 
     Returns:
-        dict: A dictionary mapping player ID to player data (name, lines, team, etc).
+        dict: A dictionary mapping player names to their odds information from different bookmakers for one game.
     """
 
-    # PrizePicks props data API
-    URL = "https://partner-api.prizepicks.com/projections?league_id=7"
+    EVENT_ID = event_id
+    MARKETS = prop_type
 
-    try:
-        response = requests.get(URL)
-        if response.status_code != 200:
-            # if the status code is not 200, return an empty dictionary
-            print(f"Failed to retrieve data: {response.status_code}, {response.text}")
+    request_url = (
+        f"https://api.the-odds-api.com/v4/sports/{SPORT}/events/{EVENT_ID}/odds"
+    )
+
+    params = {
+        "apiKey": API_KEY,
+        "regions": REGIONS,
+        "markets": MARKETS,
+        "oddsFormat": ODDS_FORMAT,
+    }
+
+    response = requests.get(request_url, params=params)
+
+    players_odds_all_books = {}
+
+    if response.status_code == 200:
+        odds_data = response.json()
+
+        if not odds_data.get("bookmakers"):
             return {}
 
-        try:
-            prizepicks_data = response.json()
-        except ValueError as e:
-            # handle cases where the response is not in valid JSON format
-            print(f"Error parsing JSON response: {e}")
-            return {}
+        home_team = odds_data.get("home_team")
+        away_team = odds_data.get("away_team")
 
-        if not prizepicks_data.get("data"):
-            # check if the 'data' key is present and not empty
-            print("No PrizePicks Data...")
-            return {}
-
-        # process the PrizePicks data
-        lines = prizepicks_data["data"]
-        players_lines = {
-            player["id"]: player["attributes"]
-            for player in prizepicks_data["included"]
-            if player["type"] == "new_player"
-        }
-
-        # initialize lines for each player
-        for player_id in players_lines:
-            players_lines[player_id]["lines"] = {}
-
-        for line in lines:
-            attributes = line["attributes"]
-            odds_type = attributes.get("odds_type")
-
-            # skip demon and goblin props
-            if odds_type in ["demon", "goblin"]:
+        for bookmaker in odds_data["bookmakers"]:
+            bookmaker_name = bookmaker["key"]
+            if bookmaker_name in ["betrivers", "unibet_us"]:
                 continue
 
-            player_id = line["relationships"]["new_player"]["data"]["id"]
-            stat_type = attributes["stat_type"]
-            stat_line = attributes["line_score"]
+            for market in bookmaker["markets"]:
+                if market["key"] == MARKETS:
+                    for outcome in market["outcomes"]:
+                        player_name = outcome["description"]
+                        if player_name not in players_odds_all_books:
+                            players_odds_all_books[player_name] = {
+                                "home_team": home_team,
+                                "away_team": away_team,
+                            }
 
-            if player_id in players_lines:
-                players_lines[player_id]["lines"][stat_type] = stat_line
+                        if bookmaker_name not in players_odds_all_books[player_name]:
+                            players_odds_all_books[player_name][bookmaker_name] = {
+                                "points": outcome["point"],
+                                "overOdds": None,
+                                "underOdds": None,
+                            }
 
-        # print("getPrizePicksData():\n\n\n")
-        # print(players_lines)
-        # print("\n\n\n")
-        return players_lines
+                        if "over" in outcome["name"].lower():
+                            players_odds_all_books[player_name][bookmaker_name][
+                                "overOdds"
+                            ] = outcome["price"]
+                        elif "under" in outcome["name"].lower():
+                            players_odds_all_books[player_name][bookmaker_name][
+                                "underOdds"
+                            ] = outcome["price"]
+    else:
+        print(f"Failed to retrieve data: {response.status_code}, {response.text}")
 
-    except requests.RequestException as e:
-        print(f"Request failed: {e}")
-        return {}
+    return players_odds_all_books
 
 
 def calculate_implied_probability(odds):
@@ -143,7 +215,6 @@ def normalize_name(name):
     name_replacements = {
         "CJ": "C.J.",
         "Herbert": "Herb",
-        # add other names if needed
     }
     for key, value in name_replacements.items():
         name = name.replace(key, value)
@@ -165,99 +236,7 @@ def build_prizepicks_index(prizepicks_data):
         for pp in prizepicks_data.values():
             normalized_name = normalize_name(pp["name"])
             prizepicks_index[normalized_name] = pp
-
-    # print("PrizePicksIndex:\n\n\n")
-    # print(prizepicks_index)
-    # print("\n\n\n")
     return prizepicks_index
-
-
-def getPlayersPropsOddsForGame(event_id, prop_type):
-    """
-    Retrieves betting odds for specified player propositions (e.g., points, assists, rebounds) from different bookmakers for a specific game.
-
-    Parameters:
-        event_id (str): The unique ID for the game.
-        prop_type (str): The type of player prop to retrieve odds for (e.g., "player_points", "player_assists", "player_rebounds").
-
-    Returns:
-        dict: A dictionary mapping player names to their odds information from different bookmakers for one game.
-    """
-
-    EVENT_ID = event_id
-    MARKETS = prop_type
-
-    request_url = (
-        f"https://api.the-odds-api.com/v4/sports/{SPORT}/events/{EVENT_ID}/odds"
-    )
-
-    params = {
-        "apiKey": API_KEY,
-        "regions": REGIONS,
-        "markets": MARKETS,
-        "oddsFormat": ODDS_FORMAT,
-    }
-
-    response = requests.get(request_url, params=params)
-
-    # Initialize a dictionary to hold player odds from all bookmakers
-    players_odds_all_books = {}
-
-    if response.status_code == 200:
-        odds_data = response.json()
-
-        # check if bookmakers data is present, if not exit early
-        if not odds_data.get("bookmakers"):
-            return {}
-
-        # Extract home and away team names
-        home_team = odds_data.get("home_team")
-        away_team = odds_data.get("away_team")
-
-        # Iterate over all bookmakers in the response
-        for bookmaker in odds_data["bookmakers"]:
-            bookmaker_name = bookmaker["key"]
-            # Skip these 2 books (weird odds)
-            if bookmaker_name in ["betrivers", "unibet_us"]:
-                continue
-
-            for market in bookmaker["markets"]:
-                if market["key"] == MARKETS:
-                    for outcome in market["outcomes"]:
-                        player_name = outcome["description"]
-                        if player_name not in players_odds_all_books:
-                            players_odds_all_books[player_name] = {
-                                "home_team": home_team,
-                                "away_team": away_team,
-                            }
-
-                        if bookmaker_name not in players_odds_all_books[player_name]:
-                            players_odds_all_books[player_name][bookmaker_name] = {
-                                "points": outcome["point"],
-                                "overOdds": None,
-                                "underOdds": None,
-                            }
-
-                        # assign overOdds and underOdds
-                        if "over" in outcome["name"].lower():
-                            players_odds_all_books[player_name][bookmaker_name][
-                                "overOdds"
-                            ] = outcome["price"]
-                        elif "under" in outcome["name"].lower():
-                            players_odds_all_books[player_name][bookmaker_name][
-                                "underOdds"
-                            ] = outcome["price"]
-    else:
-        print(f"Failed to retrieve data: {response.status_code}, {response.text}")
-
-    # Print the remaining and used request counts
-    # print("Remaining requests:", response.headers.get("x-requests-remaining"))
-    # print("Used requests:", response.headers.get("x-requests-used"))
-
-    # print("getPlayersPropsOddsForGame(): " + prop_type + " \n\n\n")
-    # print(players_odds_all_books)
-
-    return players_odds_all_books
 
 
 def find_best_props(players_data, prop_type, prizepicks_index, include_prizepicks):
@@ -340,7 +319,6 @@ def find_best_props(players_data, prop_type, prizepicks_index, include_prizepick
                         else "under"
                     )
 
-                    # calculate the weighted average best bet probability
                     weighted_sum = 0
                     total_weight = 0
                     for book_odds in matching_props:
@@ -391,7 +369,6 @@ def find_best_props(players_data, prop_type, prizepicks_index, include_prizepick
                     else "under"
                 )
 
-                # Calculate the weighted average best bet probability
                 weighted_sum = 0
                 total_weight = 0
                 for book_odds in player_props:
@@ -446,29 +423,23 @@ def getBestProps():
         "player_rebounds_assists",
     ]
 
-    # get todays NBA games
     games_today = getGames()
     if not games_today:
         return {"message": "No NBA games.", "data": []}
 
-    # get prizepicks data
     prizepicks_data = getPrizePicksData()
     if not prizepicks_data:
         return {"message": "No Props available on PrizePicks.", "data": []}
 
-    # buld prizepicks index
     prizepicks_index = build_prizepicks_index(prizepicks_data)
 
-    # list to hold all player props
     all_best_props = []
 
-    # test for specific games
     specific_games = [
         "3b6b51e8f40c2d76be4d907339eaa27b",
     ]
 
     for game_id in games_today:
-        # if game_id in specific_games:
         for prop_type in prop_types:
             player_props_odds_for_game = getPlayersPropsOddsForGame(game_id, prop_type)
             best_props = find_best_props(
@@ -479,12 +450,10 @@ def getBestProps():
             )
             all_best_props.extend(best_props.values())
 
-    # sort props list by bestBetProbability in descending order (best bet on top)
     sorted_best_props = sorted(
         all_best_props, key=lambda x: x["bestBetProbability"], reverse=True
     )
 
-    # return best x props
     return {"message": "Success", "data": sorted_best_props[:27]}
 
 
