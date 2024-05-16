da233d781ec0f034cb37444a46be68da3f120757
Sameer Fidai <sameer.fidai09@gmail.com>
2024-05-16 15:38:15 -0400

    UI changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ export default function HomePage() {
       <Head>
         <title>FiveLegFlex</title>
       </Head>
-      <div className="bg-cover bg-center">
-        <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
+      <div className="bg-cover bg-center overflow-hidden">
+        <div className="flex flex-col items-center justify-center h-full text-center p-4">
           <h1 className="text-7xl font-bold mb-4 text-white shadow">FiveLegFlex</h1>
           <p className="text-2xl text-gold mb-6 animate-pulse font-light tracking-wide">The Ultimate NBA Player Prop Betting Experience</p>
           <Link href="/props" className="px-7 py-2 bg-gold text-black rounded-full font-bold text-lg hover:bg-opacity-50 transition duration-150">
