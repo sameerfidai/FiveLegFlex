9ff283657c1ad1daae8af53518b57429a2684ffb
Sameer Fidai <sameer.fidai09@gmail.com>
2024-05-25 11:36:44 -0400

    ui changes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
       <Head>
         <title>Contact Us | FiveLegFlex</title>
       </Head>
-      <div className="text-white p-4 md:p-10 lg:p-12">
+      <div className="text-white p-6 md:p-10 lg:p-12">
         <div className="max-w-4xl mx-auto">
           <h1 className="text-center text-5xl font-bold mb-6">Contact Us</h1>
           <p className="text-center text-xl mb-4">Have questions or need support? Get in touch with us.</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const HomePage = () => {
       <Head>
         <title>FiveLegFlex</title>
       </Head>
-      <div className="bg-cover bg-center overflow-hidden">
+      <div className="">
         <div className="flex flex-col items-center justify-center h-full text-center p-4">
           <h1 className="text-7xl font-bold mb-4 text-white shadow">FiveLegFlex</h1>
           <p className="text-2xl text-gold mb-6 animate-pulse font-light tracking-wide">NBA Player Prop Optimizer</p>
