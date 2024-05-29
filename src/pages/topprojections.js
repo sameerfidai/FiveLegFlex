9a276a6b1dc7bdc058f24af14c532b11bf872857
Sameer Fidai <sameer.fidai09@gmail.com>
2024-05-28 20:00:51 -0400

    remove scrape.js

--- src/pages/topprojections.js
+++ src/pages/topprojections.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import Layout from "@/components/Layout";
 import Head from "next/head";
-import axios from "axios";
+import { Builder, By } from "selenium-webdriver";
+import chrome from "selenium-webdriver/chrome";
 
 const TopProjections = ({ projections }) => {
   const [selectedIndices, setSelectedIndices] = useState([]);
@@ -62,12 +63,39 @@ const TopProjections = ({ projections }) => {
 };
 
 export async function getServerSideProps() {
+  const options = new chrome.Options();
+  options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+  let driver = new Builder().forBrowser("chrome").setChromeOptions(options).build();
+
   let projections = [];
+
   try {
-    const { data } = await axios.get("https://five-leg-flex.vercel.app/api/scrape");
-    projections = data;
+    await driver.get("https://www.bettingpros.com/nba/picks/prop-bets/");
+    await driver.sleep(2000); // Wait for the page to load
+
+    let contentDivs = await driver.findElements(By.css("div.grouped-items-with-sticky-footer__content"));
+    for (let i = 0; i < Math.min(contentDivs.length, 10); i++) {
+      let content = contentDivs[i];
+      let player = await content.findElement(By.css("a.link.pbcs__player-link")).getText();
+      let ftsy_score = await content.findElement(By.css("div.flex.card__prop-container span.typography")).getText();
+      let proj = await content.findElement(By.css("div.flex.card__proj-container span.typography")).getText();
+      let diff = await content.findElement(By.css("div.flex.card__proj-container span.typography:nth-child(2)")).getText();
+      let recommendation = await content.findElement(By.css("div.flex.card__proj-container span.projection__recommendation")).getText();
+      let img_url = await content.findElement(By.css("img.player-image-card__player-image")).getAttribute("src");
+
+      projections.push({
+        player: player.trim(),
+        ftsy_score: ftsy_score.trim(),
+        projection: proj.trim().replace("Proj ", ""),
+        difference: diff.trim().replace("Diff ", ""),
+        recommendation: recommendation.trim(),
+        img_url: img_url.trim(),
+      });
+    }
   } catch (error) {
     console.error("Error fetching projections:", error);
+  } finally {
+    await driver.quit();
   }
 
   return { props: { projections } };
