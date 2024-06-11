0999bd48dd983018f566ba1fbd5e6377886c96d4
Sameer Fidai <sameer.fidai09@gmail.com>
2024-06-10 20:27:07 -0400

    scrollbar and alignment fix

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -26,8 +26,8 @@ const Layout = ({ children }) => {
 
   return (
     <div className="flex flex-col min-h-screen bg-fullblack text-white">
-      <nav className="bg-fullblack bg-opacity-60 backdrop-blur-lg w-full sticky top-0 z-50 px-5 sm:px-6 lg:px-8 py-4 border-b border-black2">
-        <div className="max-w-7xl mx-auto flex justify-between items-center">
+      <nav className="bg-fullblack bg-opacity-60 backdrop-blur-lg w-full sticky top-0 z-50 py-4 border-b border-black2">
+        <div className="container mx-auto px-5 sm:px-6 lg:px-8 flex justify-between items-center">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <Link href="/" className="text-xl font-bold text-white hover:text-gold transition duration-300 ease-in-out">
               FiveLegFlex
