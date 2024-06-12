5f968c3799cf43cd06be2e43b1067c332630bf1b
Sameer Fidai <sameer.fidai09@gmail.com>
2024-06-12 16:34:17 -0400

    add navbar on not found page

--- frontend/src/app/not-found.js
+++ frontend/src/app/not-found.js
@@ -1,13 +1,20 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-fullblack text-white">
-      <h1 className="text-6xl font-bold mb-4">FiveLegFlex</h1>
-      <p className="text-xl mb-8">Oops! The page you're looking for doesn't exist.</p>
-      <Link href="/" className="px-6 py-3 bg-white text-black rounded-md hover:bg-black2 hover:text-white transition duration-200">
-        Go back Home
-      </Link>
-    </div>
+    <Layout>
+      <Head>
+        <title>Not Found</title>
+      </Head>
+      <div className="h-full flex flex-col items-center justify-center bg-fullblack text-white">
+        <h1 className="text-6xl font-bold mb-4 text-white shadow">Not Found</h1>
+        <p className="text-xl text-gold mb-4">Oops! The page you're looking for doesn't exist.</p>
+        <Link href="/" className="px-6 py-3 bg-white text-black rounded-md hover:bg-black2 hover:text-white transition duration-200">
+          Go back Home
+        </Link>
+      </div>
+    </Layout>
   );
 }
--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { useState, useEffect, useRef } from "react";
 import "../app/globals.css";
