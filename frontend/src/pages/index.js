ab87bcacf625117aac44a1fdce841851bda8ba86
Sameer Fidai <sameer.fidai09@gmail.com>
2024-06-26 18:46:41 -0400

    drop shadow

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -11,7 +11,7 @@ const HomePage = () => {
       <div className="w-full flex flex-col justify-center items-center bg-offwhite dark:bg-fullblack">
         <div className="text-center p-4">
           <h1 className="text-6xl font-bold mb-4 text-fullblack dark:text-white">FiveLegFlex</h1>
-          <p className="text-2xl text-gold animate-pulse font-light tracking-wide">Player Props Optimizer</p>
+          <p className="text-2xl text-gold animate-pulse font-light tracking-wide drop-shadow-sm">Player Props Optimizer</p>
           {/* <Link href="/props">
             <div className="px-7 py-2 bg-gold text-fullblack dark:text-white rounded-full font-bold text-lg hover:bg-opacity-50 hover:text-white transition duration-150 cursor-pointer">View Props</div>
           </Link> */}
