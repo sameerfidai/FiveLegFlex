fc24bece13ccb0b2a9577e8a66e98f0c21152b59
Sameer Fidai <sameer.fidai09@gmail.com>
2024-07-23 21:09:43 -0400

    update about page

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -10,9 +10,13 @@ const About = () => {
       </Head>
       <div className="bg-offwhite dark:bg-fullblack text-fullblack dark:text-white p-6 md:p-10 lg:p-12">
         <div className="max-w-4xl mx-auto">
-          <h1 className="text-center text-5xl font-bold mb-6 text-fullblack dark:text-white">About FiveLegFlex</h1>
+          <h1 className="text-center text-5xl font-bold mb-6 text-fullblack dark:text-white">
+            About FiveLegFlex
+          </h1>
           <p className="text-center text-xl mb-4 text-fullblack dark:text-white">
-            FiveLegFlex provides real-time data and forecasts for NBA and soccer player prop bets. Our platform helps you make informed betting decisions and maximize your potential for high-value outcomes. We're also excited to expand to more sports in the future.
+            FiveLegFlex provides real-time data and forecasts for player prop
+            bets, helping you make informed decisions and maximize your
+            potential for high-value outcomes.
           </p>
         </div>
       </div>
